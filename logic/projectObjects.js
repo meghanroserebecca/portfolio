3e940d00512671ab7f9d692cfe91721494ff322c
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-13 20:28:55 -0800

    3 project objects created

---
+++ logic/projectObjects.js
@@ -0,0 +1,25 @@
+var projectObject [
+  {
+    title: 'about me',
+    category:    'early work',
+    gitHubUrl:   'https://github.com/meghanroserebecca/about_me',
+    latestCommit: '2016-11-13',
+    description: '<p>A learning repository from my first Code Fellows course: a project involving HTML, CSS and vanilla Javascript, inspired in no small part by "The Princess Bride."</p>'
+  },
+
+  {
+    title: 'salmon cookies',
+    category:    'early work',
+    gitHubUrl:   'https://github.com/meghanroserebecca/cookie-stand',
+    latestCommit: '2016-11-15',
+    description: '<p>Cookie Stand, aka Salmon Cookies, is an early learning project exploring DOM manipulation, Javascript and CSS</p>'
+  },
+
+  {
+    title: 'LyricLibs',
+    category:    'early work',
+    gitHubUrl:   'https://github.com/Team-Lyric/madlib',
+    latestCommit: '2016-12-15',
+    description: '<p>This was a collaborative final project for the Code Fellows Code201 course, bringing together the knowledge gleaned from the entire course. Javascript, DOM manipulation, local storage, CSS and graphic design work are featured here in an app designed around the concept of Mad Libs&trade, but with lyrics to popular songs.</p>'
+  },
+];
