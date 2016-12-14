9cd56cd76541c9ebcc105950e5c4597a8964324c
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-13 22:47:00 -0800

    modified js and index to fix small issues with porfolio; added info to .gitignore

--- logic/projects.js
+++ logic/projects.js
@@ -3,17 +3,17 @@ var projects = [];
 function Project (opts) {
   this.title = opts.title;
   this.category = opts.category;
-  this.gitHubUrl = opts.gitHubUrl;
   this.latestCommit = opts.latestCommit;
+  this.gitHubUrl = opts.gitHubUrl;
   this.description = opts.description;
 }
 
 Project.prototype.toHTML = function() {
   var $newProject = $('article.template').clone();
   $newProject.find('h3').html(this.title);
-  $newProject.find('section.categor').html(this.category);
+  $newProject.find('div.category').html(this.category);
   $newProject.find('div.latestCommit').html(this.latestCommit);
-  $newProject.find('a').html(this.gitHubUrl);
+  $newProject.find('a').attr('href', this.gitHubUrl);
   $newProject.find('section.description').html(this.description);
 
   $newProject.removeAttr('class');
